48b49b95dc00e5f557f4daa59488e0b671e3d27a
Subhamp7 <Subham306952@gmail.com>
2020-06-14 01:07:45 +0530

    changes the output from boolean to string

--- flask_app.py
+++ flask_app.py
@@ -21,14 +21,15 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    final_features = (np.array(int_features)).reshape(1,-1)
     prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html',output)
+    if(prediction==0):
+        output='Hurray!!! You are Safe'
+    else:
+        output='OOPS!!! You need medical attention'
+    return render_template('index.html', prediction_text=output)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
 
