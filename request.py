8043633000f22735e1f1672ac4bac9355ef6741d
Subhamp7 <Subham306952@gmail.com>
2020-09-13 00:26:40 +0530

    updating

--- request.py
+++ request.py
@@ -3,17 +3,16 @@ import requests
 url = "http://localhost:5000/predict"
 r = requests.post(url,json={"Age":45, 
                             "Gender":1,
-                            "Chest pain type":3 ,
-			                "Resting blood pressure":120, 
-                            "Serum cholestoral in mg/dl":110, 
-                            "Fasting blood sugar":1,
-                			"Resting electrocardiographic results":1, 
-                            "Maximum heart rate achieved":130, 
-                            "Exercise induced angina":1 ,
-                            "oldpeak = ST depression induced by exercise relative to rest":26,
-                            "The slope of the peak exercise ST segment":1,
-                            "Number of major vessels colored by flourosopy":3,
-                            "Thal":1,
-                            "Diagnosis of heart disease (angiographic disease status)":0})
+                            "CP":3 ,
+                            "RBP":120, 
+                            "SC":110, 
+                            "FBS":1,
+                            "RER":1, 
+                            "MHR":130, 
+                            "EIA":1 ,
+                            "ST":26,
+                            "SST":1,
+                            "Flourosopy":3,
+                            "Thal":1})
 
 print(r.json())
