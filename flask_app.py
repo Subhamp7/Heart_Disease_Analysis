5e19a444ca123f6b8bf1ca53b2ccf6b1bf219be2
Subhamp7 <Subham306952@gmail.com>
2020-09-13 20:04:56 +0530

    added try and except for better experience

--- flask_app.py
+++ flask_app.py
@@ -13,7 +13,6 @@ app   = Flask(__name__,static_url_path='/static')
 #loading the models
 model = pickle.load(open('ml_model.pkl', 'rb'))
 enco  = pickle.load(open('encoder.pkl',  'rb'))
-scal  = pickle.load(open('scaling.pkl',  'rb'))
 
 @app.route('/')
 def home():
@@ -24,23 +23,22 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    
-    attibutes        = ["Age", "Gender","CP", "RBP", "SC", "FBS","RER", 
-                        "MHR", "EIA", "ST", "SST", "Flourosopy", "Thal"]
-    attibutes_val    = [float(request.form[items]) for items in attibutes]
-    attibutes_array  = (numpy.array(attibutes_val)).reshape(1,-1)
-    attibutes_encode = enco.transform(attibutes_array)
-  
-
-    prediction       = model.predict(attibutes_encode)
-    
-    if(prediction==1):
-        output = 'OOPS!!! You need medical attention'
-    else:
-        output = 'Hurray!!! You are Safe'
-    
+    try:
+        attibutes        = ["Age", "Gender","CP", "RBP", "SC", "FBS","RER", 
+                            "MHR", "EIA", "ST", "SST", "Flourosopy", "Thal"]
+        attibutes_val    = [float(request.form[items]) for items in attibutes]
+        attibutes_array  = (numpy.array(attibutes_val)).reshape(1,-1)
+        attibutes_encode = enco.transform(attibutes_array)
+        prediction       = model.predict(attibutes_encode)
+        
+        if(prediction==1):
+            output = 'OOPS!!! You need medical attention'
+        else:
+            output = 'Hurray!!! You are Safe'
+    except:
+        output="Sorry, some internal issue occured, Please try again, if the issue presist inform via LinkedIn"
         
     return render_template('index_predict.html', prediction_text=output)
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
